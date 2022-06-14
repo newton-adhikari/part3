380698a1803a6b403f9255ad16309cd5b418b764
NewtonAdhikari <newtonadk@gmail.com>
2022-06-14 16:29:15 +0545

    phonebook step8 completed

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+morgan.token('body', req => {
+    return JSON.stringify(req.body)
+})  
+
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'))
 
 let persons = [
     { 
