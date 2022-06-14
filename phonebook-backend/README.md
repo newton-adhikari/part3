cd69271bbb2c54188e04ca09271b0ba3708c015e
NewtonAdhikari <newtonadk@gmail.com>
2022-06-14 23:33:42 +0545

    phonebook backend step11 completed

--- phonebook-backend/README.md
+++ phonebook-backend/README.md
@@ -1,2 +1,2 @@
 The live backend can be found at the following link
-https://stark-journey-32696.herokuapp.com/api/persons
+https://stark-journey-32696.herokuapp.com
--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -7,6 +7,7 @@ morgan.token('body', req => {
     return JSON.stringify(req.body)
 })  
 
+app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'))
